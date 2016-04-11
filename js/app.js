a26fca9522048c54709d8173f2ce735ef211ea4e
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2016-04-10 22:03:54 -0700

    Made new game function to reset stats and start a new game

--- js/app.js
+++ js/app.js
@@ -1,17 +1,35 @@
 
 $(document).ready(function(){
-	
+	//call new game function on load and when clicked
+	newGame();
+	$(".new").click(function(){
+		newGame();
+	});
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
-
   	});
 
   	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
-
 });
+
+/*Starts a new game and resets previous stats*/ 
+function newGame(){
+	var guessNum = 0;	
+	var secretNum = getRandom();
+	var guesses = [];
+
+	$("#feedback").text("Make your Guess!");
+	$("#count").text(guessNum);
+	$("#guessList").html("");
+}
+
+function getRandom(){
+	return Math.floor(Math.random() * (100)) + 1;
+}
 
 
