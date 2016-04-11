328bbef7e1cfef621d4a237c15513be1b56239b0
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2016-04-11 02:30:16 -0700

    Added user input validation

--- js/app.js
+++ js/app.js
@@ -3,9 +3,22 @@ $(document).ready(function(){
 	var guessNum, secretNum, guesses;
 
 	/*call new game function on load and when clicked*/
-	newGame();
 	$(".new").click(function(){
 		newGame();
+	});
+
+	//Grabs user input, checks to see if valid and continues with game.
+	$("#guessButton").click(function(e){
+		e.preventDefault();
+		var $userGuess = $("#userGuess").val();
+		$("#userGuess").val("");
+		
+		if($userGuess === "" || !isValid(+$userGuess)){
+			$("#feedback").text("Enter a number from 0 to 100");
+		 	return;
+		}
+		
+		console.log("valid");
 	});
 
 	/*--- Display information modal box ---*/
@@ -29,8 +42,17 @@ $(document).ready(function(){
 	}
 });
  
+//Random number from 0 to 100
 function getRandom(){
-	return Math.floor(Math.random() * (100)) + 1;
+	return Math.floor(Math.random() * 101);
+}
+
+//Check user input and return if its valid or not
+function isValid(num){
+	if(isNaN(num) || num > 100 || num < 0 || num % 1 !== 0){
+		return false;
+	}
+	return true;
 }
 
 
